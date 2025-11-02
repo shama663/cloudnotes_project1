a2592125387da219567276fcc04a695492f33677
Your Name <youremail@example.com>
2025-11-03 03:51:12 +0530

    fix: added base path for gh-pages

--- vite.config.mjs
+++ vite.config.mjs
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/cloudnotes_project1/', // ✅ your repo name here
+  base: './', // 👈 important for GitHub Pages
 })
